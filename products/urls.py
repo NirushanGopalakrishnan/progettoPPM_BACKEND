0fcbfcfeec02bec975a5a233a14862cb9ee665df
NirushanGopalakrishnan <nirushan.gopalakrishnan@edu.unifi.it>
2025-06-30 22:30:50 +0200

    gestione login ecc

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductListCreateView, CartView, AddToCartView
 
 urlpatterns = [
-    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('', ProductListCreateView.as_view(), name='product-list-create'),
     path('cart/', CartView.as_view(), name='cart'),
     path('cart/add/', AddToCartView.as_view(), name='add-to-cart'),
 ]
